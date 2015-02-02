b9862b45d621c33eb18d09c5f9b830148846068c
Arek Kaleta <arekkaleta95@gmail.com>
2015-02-02 12:39:24 +0100

    fix project

--- gen/com/example/weatherapp/R.java
+++ gen/com/example/weatherapp/R.java
@@ -24,18 +24,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout2=0x7f080002;
+        public static final int RelativeLayout1=0x7f080000;
+        public static final int action_settings=0x7f08000a;
+        public static final int detailWeatherList=0x7f080001;
+        public static final int imageView1=0x7f080007;
+        public static final int tempView=0x7f080004;
+        public static final int textView1=0x7f080008;
+        public static final int textView2=0x7f080005;
+        public static final int textView3=0x7f080006;
+        public static final int textView4=0x7f080009;
+        public static final int weatherView=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_detail_weather=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int dayweatherrecord=0x7f030002;
+        public static final int weather_activity=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int detail_weather=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_detail_weather=0x7f050003;
     }
     public static final class style {
         /** 

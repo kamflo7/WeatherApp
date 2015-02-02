bf047d8ed0484f804c706d61ae67184259f7388e
Kamil Florczyk <florczykkamil@gmail.com>
2015-02-02 12:53:13 +0100

    Changes after rebase

--- gen/com/example/weatherapp/R.java
+++ gen/com/example/weatherapp/R.java
@@ -24,27 +24,32 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout2=0x7f080002;
+        public static final int LinearLayout2=0x7f080003;
         public static final int RelativeLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000a;
+        public static final int action_settings=0x7f08000b;
         public static final int detailWeatherList=0x7f080001;
-        public static final int imageView1=0x7f080007;
-        public static final int tempView=0x7f080004;
-        public static final int textView1=0x7f080008;
-        public static final int textView2=0x7f080005;
-        public static final int textView3=0x7f080006;
-        public static final int textView4=0x7f080009;
-        public static final int weatherView=0x7f080003;
+        public static final int imageView1=0x7f080008;
+        public static final int pager=0x7f080002;
+        public static final int tempView=0x7f080005;
+        public static final int textView1=0x7f080009;
+        public static final int textView2=0x7f080006;
+        public static final int textView3=0x7f080007;
+        public static final int textView4=0x7f08000a;
+        public static final int weatherView=0x7f080004;
     }
     public static final class layout {
         public static final int activity_detail_weather=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int dayweatherrecord=0x7f030002;
-        public static final int weather_activity=0x7f030003;
+        public static final int activity_view_hourly=0x7f030002;
+        public static final int activity_view_long=0x7f030003;
+        public static final int activity_view_now=0x7f030004;
+        public static final int dayweatherrecord=0x7f030005;
+        public static final int weather_activity=0x7f030006;
     }
     public static final class menu {
         public static final int detail_weather=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int view_first=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;

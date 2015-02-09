72446eae8017cb1108ec677d8801f633e33241e2
Paul Brewczynski <pbbluesm@gmail.com>
2015-02-09 10:41:28 +0100

    Changed target to 21 (for my purposes). And added two API classes.

---
+++ src/models/APIJSONAsyncTask.java
@@ -0,0 +1,16 @@
+package models;
+
+import android.os.AsyncTask;
+
+
+public class APIJSONAsyncTask extends AsyncTask<String, String, Void> {
+
+	@Override
+	protected Void doInBackground(String... arg0) {
+		String apiURL = 
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

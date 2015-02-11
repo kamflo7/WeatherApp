48d7f8dae5a54d3cfe9c553db8f148804a3988f2
Paul Brewczynski <pbbluesm@gmail.com>
2015-02-11 09:11:24 +0100

    Sketch of AsyncTask with URL connection

--- src/models/APIJSONAsyncTask.java
+++ src/models/APIJSONAsyncTask.java
@@ -1,16 +1,66 @@
 package models;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.os.AsyncTask;
+import android.util.Log;
 
 
 public class APIJSONAsyncTask extends AsyncTask<String, String, Void> {
 
+	@Override 
+	protected void onPreExecute() {
+		Log.d("TAG", "We've starte request");
+	}
+
 	@Override
 	protected Void doInBackground(String... arg0) {
-		String apiURL = 
+		String apiURL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&mode=json";
 		
-		// TODO Auto-generated method stub
+		
+		try {
+			URL u = new URL(apiURL);
+			HttpURLConnection c = (HttpURLConnection) u.openConnection();
+			c.setRequestMethod("GET");
+			c.setRequestProperty("Content-length", "0");
+			c.setUseCaches(false);
+			c.connect();
+			
+			int status = c.getResponseCode();
+			
+			switch (status) {
+			case 200:
+			case 201:
+				BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+				StringBuilder sb = new StringBuilder();
+				String line;
+				
+				while((line = br.readLine()) != null) {
+					Log.d("TAG", line);
+				}
+				br.close();
+			}
+			
+		} catch (MalformedURLException ex) {
+			
+		} catch (IOException ex) {
+			
+		}
+		
 		return null;
 	}
+	
+	
+	
+	
 
 }

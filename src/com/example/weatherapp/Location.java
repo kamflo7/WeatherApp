2023605a26845d060895d51fdecfa8c2a791df3d
Kamil Florczyk <florczykkamil@gmail.com>
2015-03-09 12:54:37 +0100

    Changed action bar items, now are 2 icons. Added dialog (location changer) to icon in action bar.

---
+++ src/com/example/weatherapp/Location.java
@@ -0,0 +1,13 @@
+package com.example.weatherapp;
+
+public class Location {
+	public String locationName;
+	public double latitude;
+	public double longitude;
+
+	public Location(String locationName, double latitude, double longitude) {
+		this.locationName = locationName;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+}
--- src/com/example/weatherapp/MainActivity.java
+++ src/com/example/weatherapp/MainActivity.java
@@ -1,49 +1,60 @@
 package com.example.weatherapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
 	private MyFragmentPageAdapter pagerAdapter;
 	private ViewPager mViewPager;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        final ActionBar actionBar = getActionBar();
-        
-        pagerAdapter = new MyFragmentPageAdapter(getSupportFragmentManager());
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(pagerAdapter);
-        mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+	private List<Location> locations = new ArrayList<Location>();
+	private int selectedIndexLocation = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		final ActionBar actionBar = getActionBar();
+
+		pagerAdapter = new MyFragmentPageAdapter(getSupportFragmentManager());
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(pagerAdapter);
+		mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
 			@Override
 			public void onPageSelected(int i) {
 				actionBar.setSelectedNavigationItem(i);
 			}
-			
+
 			@Override
-			public void onPageScrolled(int arg0, float arg1, int arg2) { }
-			
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
 			@Override
-			public void onPageScrollStateChanged(int arg0) { }
+			public void onPageScrollStateChanged(int arg0) {
+			}
 		});
-        
-        
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
 			@Override
 			public void onTabReselected(Tab tab, FragmentTransaction ft) {
-				
+
 			}
 
 			@Override
@@ -53,21 +64,74 @@ public class MainActivity extends FragmentActivity {
 
 			@Override
 			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-				
+
 			}
-        };
-        
-        actionBar.addTab(actionBar.newTab().setText("Teraz").setTabListener(tabListener));
-        actionBar.addTab(actionBar.newTab().setText("Godzinowa").setTabListener(tabListener));
-        actionBar.addTab(actionBar.newTab().setText("D³ugoterminowa").setTabListener(tabListener));
-    }
+		};
 
+		actionBar.addTab(actionBar.newTab().setText("Teraz")
+				.setTabListener(tabListener));
+		actionBar.addTab(actionBar.newTab().setText("Godzinowa")
+				.setTabListener(tabListener));
+		actionBar.addTab(actionBar.newTab().setText("D³ugoterminowa")
+				.setTabListener(tabListener));
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	Log.d("test", "onCreateOptionMenu siê odpali³o");
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		locations.add(new Location("Kraków", 50.0605, 19.9324));
+		locations.add(new Location("Szczecin", 53.4252, 14.5555));
+		locations.add(new Location("Los Angeles", 34.0535, 118.245));
+		locations.add(new Location("Miami", 25.7748, -80.1977));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		Log.d("test", "onCreateOptionMenu siê odpali³o");
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_location:
+			showDialogLocation();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void showDialogLocation() {
+		AlertDialog.Builder builderSingle = new AlertDialog.Builder(
+				MainActivity.this);
+
+		builderSingle.setIcon(R.drawable.ic_launcher);
+		builderSingle.setTitle("Select location");
+		final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+				MainActivity.this, android.R.layout.select_dialog_singlechoice);
+
+		for (int i = 0; i < locations.size(); i++) {
+			arrayAdapter.add(locations.get(i).locationName);
+		}
+
+		builderSingle.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				});
+
+		builderSingle.setAdapter(arrayAdapter,
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Toast.makeText(getApplicationContext(),
+								"You selected item " + which, Toast.LENGTH_LONG)
+								.show();
+					}
+				});
+		builderSingle.show();
+	}
+
 }

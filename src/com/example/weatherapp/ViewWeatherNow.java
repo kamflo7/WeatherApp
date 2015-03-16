805f0c6456c1bcdc80835e8d4b73ffa2be303d0b
Arek Kaleta <arekkaleta95@gmail.com>
2015-03-16 10:29:11 +0100

    finished land layout

--- src/com/example/weatherapp/ViewWeatherNow.java
+++ src/com/example/weatherapp/ViewWeatherNow.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 public class ViewWeatherNow extends Fragment {
     public static final String ARG_OBJECT = "object";

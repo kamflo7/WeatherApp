88a154cef2b17a69c522dfee19b2b214186d5fbf
Paul Brewczynski <pbbluesm@gmail.com>
2015-03-16 06:02:40 +0100

    API request created and working, project compiles

--- src/com/example/weatherapp/MainActivity.java
+++ src/com/example/weatherapp/MainActivity.java
@@ -1,15 +1,44 @@
 package com.example.weatherapp;
 
+import models.APIJSONAsyncTask;
+import android.location.Location;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	
+	private Button testButton;
+	private APIJSONAsyncTask apiHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        this.testButton = (Button) this.findViewById(R.id.requestTestButton);
+        
+        this.testButton.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View view) {
+        		
+        		Toast.makeText(MainActivity.this, "Button clicked" , Toast.LENGTH_SHORT).show();
+        	}
+        }); 
+        
+        this.apiHandler = new APIJSONAsyncTask();
+        
+        Location location = new Location("provider");
+        location.setLatitude(50.061389);
+        location.setLongitude(19.938333);
+        
+        this.apiHandler.requestBasicWeatherForToday(location);
+        
+
     }
 
 
@@ -17,6 +46,7 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
         return true;
     }
     
--- src/models/APIJSONAsyncTask.java
+++ src/models/APIJSONAsyncTask.java
@@ -2,6 +2,7 @@ package models;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -10,13 +11,27 @@ import java.net.URL;
 import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.location.Location;
 import android.os.AsyncTask;
 import android.util.Log;
 
 
+
 public class APIJSONAsyncTask extends AsyncTask<String, String, Void> {
 
+	
+	static String SERVER 				= "http://api.openweathermap.org/data/2.5/forecast/daily?";
+	static String LATITUDE_PARAM_NAME 	= "lat=";
+	static String LONGITUDE_PARAM_NAME 	= "lon=";
+	static String CNT_DAYS 				= "cnt=";
+	static String MODE					= "mode="; // always =json
+	
+	static String GET_DELIMITER = "&";
+	
 	@Override 
 	protected void onPreExecute() {
 		Log.d("TAG", "We've starte request");
@@ -24,8 +39,15 @@ public class APIJSONAsyncTask extends AsyncTask<String, String, Void> {
 
 	@Override
 	protected Void doInBackground(String... arg0) {
-		String apiURL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&mode=json";
 		
+		
+		
+		//String apiURL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&mode=json";
+		
+		String apiURL = arg0[0];
+		
+		
+		String result = null;
 		
 		try {
 			URL u = new URL(apiURL);
@@ -37,17 +59,26 @@ public class APIJSONAsyncTask extends AsyncTask<String, String, Void> {
 			
 			int status = c.getResponseCode();
 			
+			InputStream inputStream = null;
 			switch (status) {
 			case 200:
 			case 201:
 				BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
 				StringBuilder sb = new StringBuilder();
-				String line;
 				
-				while((line = br.readLine()) != null) {
-					Log.d("TAG", line);
-				}
+				String line = null;
+			    while ((line = br.readLine()) != null)
+			    {
+			        sb.append(line + "\n");
+			    }
+			    
+			    Log.e("TAG", sb.toString());
+			    result = sb.toString();
+				
 				br.close();
+
+				this.readJSON(result);
+				
 			}
 			
 		} catch (MalformedURLException ex) {
@@ -59,6 +90,46 @@ public class APIJSONAsyncTask extends AsyncTask<String, String, Void> {
 		return null;
 	}
 	
+	private void readJSON(String json) {
+		
+		try {
+			JSONObject jObject = new JSONObject(json);
+			JSONArray jArray = jObject.getJSONArray("list");
+			
+			Double doubleRain = jArray.getJSONObject(2).getDouble("rain");
+			
+			Log.e("MYTAG", "rain" + doubleRain);
+		} catch (JSONException e) {
+			Log.e("TAG", e.toString());
+		}
+		
+	}
+	
+	
+	public DayWeather requestBasicWeatherForToday(Location location) {
+		
+		
+		
+		String requestURL = 
+				   String.format(SERVER + 
+				  LATITUDE_PARAM_NAME  	+ "%f" + GET_DELIMITER + 
+				  LONGITUDE_PARAM_NAME 	+ "%f" + GET_DELIMITER +  
+				  CNT_DAYS				+ "%f" + GET_DELIMITER + 
+				  MODE					+ "json", 
+				  location.getLatitude(),
+				  location.getLongitude(),
+				  10.0);
+		
+		
+		this.execute(requestURL);
+		
+		///this.doInBackground(requestURL);
+		
+		location.getLatitude();
+		location.getLongitude();
+		
+		return new DayWeather();
+	}
 	
 	
 	
